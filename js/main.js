c810c38e9d7d1c5009eb14b3dd00ae50bfd8a1f8
dav100ide <oreglio.dev@outlook.it>
2022-10-21 18:58:25 +0200

    aggiunge prototipo calcolatore

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-const userName = document.getElementById('user-name').value;
-const km = Number(document.getElementById('km').value);
-console.log(typeof km, km, "non capisco perchè il valore è zero, anche se nell'input ho messo un Number");
+// definisco le const
+const userName = document.getElementById('user-name');
+const km = document.getElementById('km');
+const age = document.getElementById('age');
+// aggiungo gli event listeners così si aggiorna l'input
+km.addEventListener('input', getValue);
+userName.addEventListener('input', getValue);
+age.addEventListener('input', getValue);
+// nome funzione ambiguo
+function getValue() {
+   // definisco i valori degli input con le const
+   const userNameValue = userName.value;
+   const kmValue = Number(km.value);
+   const ageValue = age.value;
+   let message = `non è stato applicato lo sconto`;
+   let price = kmValue * 0.21;
+
+   if (ageValue == 'minor') {
+      price -= price * 0.2;
+      message = `è stato applicato lo sconto minorenni`;
+   } else if (ageValue == 'over-65') {
+      price -= price * 0.4;
+      message = `è stato applicato lo sconto maggiorenni`;
+   }
+   document.getElementById('show-price').addEventListener('click', function () {
+      document.getElementById('final-price').innerHTML = `il prezzo finale è di €${price.toFixed(2)} ${message}`;
+   });
+}
