0c086f69cb536ec4e965290003d1315cbb319788
dav100ide <oreglio.dev@gmail.com>
2022-10-23 12:27:00 +0200

    JS: funzione genera

--- js/main.js
+++ js/main.js
@@ -11,18 +11,29 @@ function getValue() {
    // definisco i valori degli input con le const
    const userNameValue = userName.value;
    const kmValue = Number(km.value);
-   const ageValue = age.value;
-   let message = `non è stato applicato lo sconto`;
+   const ageValue = Number(age.value);
+   let message = `Biglietto Standard`;
    let price = kmValue * 0.21;
 
    if (ageValue == 'minor') {
       price -= price * 0.2;
-      message = `è stato applicato lo sconto minorenni`;
+      message = `Offerta Minorenni`;
    } else if (ageValue == 'over-65') {
       price -= price * 0.4;
-      message = `è stato applicato lo sconto maggiorenni`;
+      message = `Offerta Over 65`;
    }
-   document.getElementById('show-price').addEventListener('click', function () {
-      document.getElementById('final-price').innerHTML = `il prezzo finale è di €${price.toFixed(2)} ${message}`;
+   document.getElementById('show-ticket').addEventListener('click', function () {
+      //display biglietto
+      document.getElementById('ticket').classList.replace('d-none', 'd-block');
+      // dati biglietto
+      document.getElementById('passenger-name').innerHTML = userNameValue;
+      document.getElementById('discount-type').innerHTML = message;
+      document.getElementById('wagon').innerHTML = Math.floor(Math.random() * 11);
+      document.getElementById('cp-code').innerHTML = Math.floor(Math.random() * 10000) + 90000;
+      document.getElementById('ticket-price').innerHTML = `€${price.toFixed(2)}`;
+   });
+   // cliccando il tasto annulla si resetta il campo input e si chiude il bliglietto
+   document.getElementById('cancel').addEventListener('click', function () {
+      document.getElementById('ticket').classList.replace('d-block', 'd-none');
    });
 }
